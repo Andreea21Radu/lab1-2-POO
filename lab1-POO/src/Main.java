a449fc63d67384bf7f75342815167ab469db157c
Andreea21Radu <98233489+Andreea21Radu@users.noreply.github.com>
2024-10-17 13:29:20 +0300

    Add files via upload

---
+++ lab1-POO/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        // ex 3
+        /*
+        boolean yes = true;
+        boolean no = false;
+        int loVal = -999;
+        int hiVal = 999;
+        double grade = 87.5;
+        double amount = 50.0;
+        String hello = "world";
+
+        System.out.println(yes == no || grade > amount);
+        System.out.println(amount == 40.0 || amount == 50.0);
+        System.out.println(hiVal != loVal || loVal < 0);
+        System.out.println(true || hello.length() > 0);
+        System.out.println(hello.isEmpty() && yes);
+        System.out.println(grade <= 100 && !false);
+        System.out.println(!yes || no);
+        System.out.println(grade > 75 && 75 > amount);
+        System.out.println(amount <= hiVal && amount >= loVal);
+        System.out.println(no && !no || yes && !yes);
+
+        // ex 4
+
+        float a = 100.25f;
+        long b = (long)a;
+        System.out.println("value of a: "+a);
+        System.out.println("value of b:" +b);
+        int c = (int)b;
+        System.out.println("value of c:"+c);
+        byte d = (byte)c;
+        System.out.println("value of d:"+d);
+
+
+        // ex 5
+        int a = 5;
+        System.out.println(a + -a - a++ % 10);
+        System.out.println(a - a + --a / 10);
+        for (int i = 2; i < 5;) {
+            i++;
+            a += a;
+        }
+        System.out.println("a = " + a);
+
+        */
+    }
+}
